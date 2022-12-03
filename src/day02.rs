686b086ef272f59608e6340d06b9a20b6cc7b82a
Evangelos Vandoros <vandvag@gmail.com>
2022-12-03 13:47:13 +0200

    Added enums day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,8 +1,22 @@
 use std::error::Error;
 use std::fs;
 
+enum RoundResult {
+    Win,
+    Tie,
+    Loss,
+}
+
+enum Move {
+    Rock,
+    Paper,
+    Scisors,
+}
+
 const FILE_PATH: &str = "inputs/day01.in";
 
 pub fn day02_solutions() -> Result<(), Box<dyn Error>> {
-    unimplemented!();
+    let contents = fs::read_to_string(FILE_PATH)?;
+    let rounds_as_string = contents.split("\n").collect::<Vec<&str>>();
+    Ok(())
 }
